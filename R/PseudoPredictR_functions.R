dbeca29f236196fb9274989ff24f9927b96c303d
Heather Welch <heather.welch@noaa.gov>
2020-02-27 11:28:39 -0800

    edots

--- R/PseudoPredictR_functions.R
+++ R/PseudoPredictR_functions.R
@@ -3,9 +3,11 @@
 #' This function allows you play some nasty beats to accompany your code.
 #' @param pseudoRas character string specifiying which built-in classical painting to use as pseudo predictor. Defaults to fathorse.
 #' @param inputRas Your prediction layer 
+#' @param mask Should the output raster be masked to your input data? 
 #' @keywords dank
 #' @details use pseudoRas="show me" to see all possible arguments for the pseudoRas param
 #' @export
+#' @return a raster object
 #' @examples
 #' # Get a fathorse raster that matches the extent, and resolution of your prediction layers 
 #' beatr("tubular")
@@ -39,6 +41,31 @@
 # pseudoRas="ship"
 # inputRas=test
 # a=PseudoLayer(inputRas = inputRas,pseudoRas="fathorse",mask = T)
+
+# x=seq(80,90,by=1)
+# y=seq(80,90,by=1)
+# 
+# df=data.frame(x=as.numeric(x),
+#               y=as.numeric(y),
+#               stringsAsFactors = F)
+# 
+# 
+# PseudoPoints=function(inputPnts,pseudoRas="fathorse"){
+#   if(pseudoRas=="show me"){
+#     print("bad_day, barrel, boat, fathorse, headless_horseman, longdog, orange_you_glad, ship")
+#   } else {
+#     image=raster(glue("inst/imgs/{pseudoRas}.jpg"))
+#     extent(image)=extent(test)
+#     crs(image)=crs(test)
+#     b=resample(image,test)
+# 
+#     return(b)
+#   }
+#   
+# }
+
+
+
 
 PseudoLayer=function(inputRas,pseudoRas="fathorse",mask=F){
   if(pseudoRas=="show me"){
